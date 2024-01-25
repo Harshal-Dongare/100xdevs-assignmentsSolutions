696e21520b7d639e6ec5b20693fd7ed2fbc89f45
Harshal-Dongare <myeducationalarena@gmail.com>
2024-01-26 01:42:03 +0530

    middleware solutions updated

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,5 +1,4 @@
-const request = require('supertest');
-const assert = require('assert');
+
 const express = require('express');
 const app = express();
 // You have been given an express server which has a few endpoints.
@@ -11,10 +10,37 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
+
 let numberOfRequestsForUser = {};
+
+// this function sets the object to empty for every one second
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
+
+app.use(function(req, res, next){
+  // stores the user id
+  const userId = req.headers["user-id"];
+
+  // from the 2nd request, control will reach here
+  if(numberOfRequestsForUser[userId]){
+    // increment the request count 
+    numberOfRequestsForUser[userId]++;
+
+    // if the request are more than 5, block the user
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("No entry");
+    }else{
+      next();
+    }
+  }
+  else{
+    // if the request is first time, then it will add the user and its request count in the numberofrequest obj
+    // it will use userId as a key and 1 as a value which is nothing but request count
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
